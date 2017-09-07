56cd9a2c6049569a4c1ec5b82dffae4244883170
Taran Rai <taranrai96@gmail.com>
2017-09-07 14:37:35 -0700

    Student.php - commit #2

--- Student.php
+++ Student.php
@@ -1,5 +1,20 @@
 <?php
 
 class Student {
-	
+
+	function __construct() {
+    	$this->surname = '';
+    	$this->first_name = '';
+    	$this->emails = array();
+    	$this->grades = array();
+	}
+
+	function add_email($which,$address) {
+    	$this->emails[$which] = $address;
+	}
+
+	function add_grade($grade) {
+    	$this->grades[] = $grade;
+	}
+
 }
